e740c3e9219c3cc69f32bfc9ef4ab5647faf39c0
snewman4 <90208190+snewman4@users.noreply.github.com>
2022-10-03 12:47:30 -0600

    Commented

--- genInput.py
+++ genInput.py
@@ -1,21 +1,22 @@
 # Generates an input file for AlgoBowl
 
 from random import randrange
-import os
 
 
 def genInput(numVars, numClauses):
-    os.remove("input.txt")
-    file = open("input.txt", "a")
+    file = open("input.txt", "w")
 
+    # Writes meta information
     file.write(str(numVars) + " " + str(numClauses) + "\n")
 
     for i in range(numClauses):
+        # Can be positive or negative
         var1 = randrange(-numVars, numVars + 1)
-        while var1 == 0:
+        while var1 == 0: # Cannot be 0
             var1 = randrange(-numVars, numVars + 1)
         
         var2 = var1
+        # Can not have two of the same variable in one clause
         while var2 == var1 or var2 == -var1 or var2 == 0:
             var2 = randrange(-numVars, numVars + 1)
         
@@ -23,4 +24,4 @@ def genInput(numVars, numClauses):
 
     file.close()
 
-genInput(numVars=2, numClauses=100)
+genInput(numVars=2, numClauses=10)
