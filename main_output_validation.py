43f4c9531352c9279e06cb9c7f2c3bec70692d41
Upasana1994 <111549190+Upasana1994@users.noreply.github.com>
2022-10-11 16:45:41 -0600

    Create main_output_validation.py

---
+++ main_output_validation.py
@@ -0,0 +1,9 @@
+import outputValidator
+
+
+for file in :                   //location of the file- needs to be updated
+	inputIndex = file             // need to know the range
+	outputValidator.validateOutputFile(
+		                                  //update or add the name of the file
+		quiet=True
+	)
